4acf3c812aff7e02354bf1fef43ccd6e56950cdf
kazurayam <kazuaki.urayama@gmail.com>
2020-12-15 11:50:34 +0900

    organize imports in open2chromebrowsersandtestbothsimultaneously

--- Scripts/main/miscellaneous/open2ChromeBrowsersAndTestBothSimultaneously/Script1607996957364.groovy
+++ Scripts/main/miscellaneous/open2ChromeBrowsersAndTestBothSimultaneously/Script1607996957364.groovy
@@ -1,31 +1,5 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
-import org.openqa.selenium.Dimension
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.Point
-import org.openqa.selenium.WebDriver
-
-import com.kms.katalon.core.webui.driver.DriverFactory
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
@@ -62,6 +36,7 @@ WebUI.delay(1)
 DriverFactory.changeWebDriver(incognitoChrome)
 WebUI.comment("switched to ${WebUI.getUrl()}")
 WebUI.navigateToUrl('http://demoaut-mimic.kazurayam.com/')
+WebUI.verifyElementPresent(button, 10, FailureHandling.STOP_ON_FAILURE)
 WebUI.click(button)
 WebUI.delay(1)
 
